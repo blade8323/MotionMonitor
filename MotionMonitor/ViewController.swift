6cfd0d0e6cbd70277f367b22c71d5c34631c3a15
blade8323 <blade8323@mail.ru>
2020-09-11 16:59:32 +0500

    Initial Commit

---
+++ MotionMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionMonitor
+//
+//  Created by Владислав Соколов on 11.09.2020.
+//  Copyright © 2020 Владислав Соколов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
