eb18bc7aed904ffbad86835226c815991844fbdb
blade8323 <blade8323@mail.ru>
2020-09-12 12:53:15 +0500

    version 2

--- MotionMonitor/ViewController.swift
+++ MotionMonitor/ViewController.swift
@@ -12,37 +12,46 @@ import CoreMotion
 class ViewController: UIViewController {
 
     @IBOutlet var gyroscopeLabel: UILabel!
-    @IBOutlet var accelerometerLable: UILabel!
+    @IBOutlet var accelerometerLabel: UILabel!
     @IBOutlet var attitudeLabel: UILabel!
     
     private let motionManager = CMMotionManager()
-    private let queue = OperationQueue()
+    private var updateTimer: Timer!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         if motionManager.isDeviceMotionAvailable {
             motionManager.deviceMotionUpdateInterval = 0.1
-            motionManager.startDeviceMotionUpdates(to: queue) { (motion, error) in
-                if let motion = motion {
-                    let rotationRate = motion.rotationRate
-                    let gravity = motion.gravity
-                    let userAcc = motion.userAcceleration
-                    let attitude = motion.attitude
-                    let gyroscopeText = String(format: "Rotation Rate:\n------------------\nx: %+.2f\ny: %+.2f\nz: %+.2f\n", rotationRate.x, rotationRate.y, rotationRate.z)
-                    let acceleratorText = String(format: "Acceleration:\n------------------\nGravity x: %+.2f\t\tUser x: %+.2f\nGravity y: %+.2f\t\tUser y: %+.2f\nGravity z: %+.2f\t\tUser z: %+.2f\n", gravity.x, userAcc.x, gravity.y, userAcc.y, gravity.z, userAcc.z)
-                    let attitudeText = String(format: "Attitude: \n------------------\nRoll: %+.2f\nPitch: %+.2f\nYaw: %+.2f\n", attitude.roll, attitude.pitch, attitude.yaw)
-                    
-                    DispatchQueue.main.async {
-                        self.gyroscopeLabel.text = gyroscopeText
-                        self.accelerometerLable.text = acceleratorText
-                        self.attitudeLabel.text = attitudeText
-                    }
-                }
+            motionManager.startDeviceMotionUpdates()
+            updateTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateDisplay), userInfo: nil, repeats: true)
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.stopDeviceMotionUpdates()
+            updateTimer.invalidate()
+            updateTimer = nil
+        }
+    }
+    
+    @objc func updateDisplay() {
+        if let motion = motionManager.deviceMotion {
+            let rotationRate = motion.rotationRate
+            let gravity = motion.gravity
+            let userAcc = motion.userAcceleration
+            let attitude = motion.attitude
+            
+            let gyroscopeText = String(format: "Rotation Rate:\n------------------\nx: %+.2f\ny: %+.2f\nz: %+.2f\n", rotationRate.x, rotationRate.y, rotationRate.z)
+            let acceleratorText = String(format: "Acceleration:\n------------------\nGravity x: %+.2f\t\tUser x: %+.2f\nGravity y: %+.2f\t\tUser y: %+.2f\nGravity z: %+.2f\t\tUser z: %+.2f\n", gravity.x, userAcc.x, gravity.y, userAcc.y, gravity.z, userAcc.z)
+            let attitudeText = String(format: "Attitude:\n------------------\nRoll: %+.2f\nPitch: %+.2f\nYaw: %+.2f\n", attitude.roll, attitude.pitch, attitude.yaw)
+            DispatchQueue.main.async {
+                self.gyroscopeLabel.text = gyroscopeText
+                self.accelerometerLabel.text = acceleratorText
+                self.attitudeLabel.text = attitudeText
             }
         }
     }
-
-
 }
 
